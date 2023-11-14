8944172655ba2081a4b2dbcba489b157c40c5ab5
Agatão <116224359+agataofrancisco@users.noreply.github.com>
2023-11-14 15:19:39 +0100

    Update loginpage.dart

--- autenticacao_2/lib/loginpage.dart
+++ autenticacao_2/lib/loginpage.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class loginpage extends StatefulWidget {
@@ -11,8 +13,22 @@ class _loginpageState extends State<loginpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Seccao iniciada')
+      appBar: AppBar(
+        title: Text('Pagina Inicial'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 150,
+            color: Colors.blue,
+          ),
+        
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+            ),
+          ),
+        ],
       ),
     ); 
   }
